79c71142bbdb0c73c668561c45d661c09122bd9f
Maximilian Tagher <feedback.tagher@gmail.com>
2014-04-21 08:25:22 -0700

    Basic operations like math and some type checking supported

--- second.hs
+++ second.hs
@@ -66,16 +66,106 @@ parseAtom = do
                 "#f" -> Bool False
                 _    -> Atom atom
 
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseListOrDottedList :: Parser LispVal
+parseListOrDottedList = do
+    char '('
+    x <- try parseList <|> parseDottedList
+    char ')'
+    return x
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
+        <|> parseString
         <|> parseNumber
+        <|> parseQuoted
+        <|> parseListOrDottedList
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
+
+-- Printing values
+
+instance Show LispVal where show = showVal
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+-- Evaling
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem),
+              ("boolean?", isBoolean),
+              ("symbol?", isAtom),
+              ("number?", isLispNumber)]
+
+isBoolean :: [LispVal] -> LispVal
+isBoolean [Bool _] = Bool True
+isBoolean _ = Bool False
+
+isAtom :: [LispVal] -> LispVal
+isAtom [Atom _] = Bool True
+isAtom _ = Bool False
+
+isLispNumber :: [LispVal] -> LispVal
+isLispNumber [Number _] = Bool True
+isLispNumber _ = Bool False
+
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n ::[(Integer, String)] in
+                          if null parsed
+                            then 0
+                            else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
 
 main :: IO ()
-main = do
-    args <- getArgs
-    putStrLn (readExpr (args !! 0))
+main = getArgs >>= print . eval . readExpr . head
